98260c73ee41c1cac2f48ba0ea7f9a114f6f7544
zonderweb <zonder2009@gmail.com>
2018-06-12 16:33:34 +0300

    12.06.2018 [16:33]

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,15 @@
-$(function() {
+// var txt = 'Используем переменную';
+// document.write(txt);
 
-	// Custom JS
+// var txt = 'Значение числа: ';
+// var num = 123;
+// document.write(txt+num);
 
-});
+// var x = 'Значение числа: ';
+// document.write(x);
+// var x = 123;
+// document.write(x);
+
+var x = '3+(5*2 + 6)/4';
+document.write(x+"=");
+document.write(eval(x));
