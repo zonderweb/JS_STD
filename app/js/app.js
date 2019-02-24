119368d5c8759194b6cc187948090eb23edda7f9
@Scriptus <zonder2009@gmail.com>
2019-02-24 20:50:27 +0200

    start l vue.js {DKA dev}

---
+++ app/js/app.js
@@ -0,0 +1,8 @@
+new Vue({
+	el: '#app',
+	data: {
+		message: 'Hello Vue.js'
+	}
+})
+
+// 16:11 https://www.youtube.com/watch?v=b6Ac0jcqJIg&t=304s
--- gulpfile.js
+++ gulpfile.js
@@ -37,7 +37,7 @@ gulp.task('styles', function() {
 gulp.task('js', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
-		'app/js/kantor.js', // Always at the end
+		'app/js/app.js', // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
 	// .pipe(uglify()) // Mifify js (opt.)
